bda34422a15bb0054325d16bf74bf193e4d44901
AbdullahAli1999 <iifamxx@gmail.com>
2023-05-28 13:31:08 -0700

    lab9f

--- get_todos.php
+++ get_todos.php
@@ -1,20 +1,16 @@
 <?php 
-function get_all_todos()
+function get_done()
 {
-    $get_all_tasks_query = "SELECT id, task, date_added, done FROM tasks WHERE done = 0;";
+    $get_all_tasks_query = "SELECT id, task, date_added, done FROM tasks WHERE done = 1;";
     $response = $GLOBALS['conn']->query($get_all_tasks_query);
     if ($response && $response->num_rows > 0) {
+        echo '<ul id="my-list">';
         while ($row = $response->fetch_array()) {
-            $response['todo_id'] = $row["id"];
-            $response['todo_item'] = $row["task"];
-            $response['todo_date'] = $row["date_added"];
-            $response['todo_is_done'] =$row["done"];
-            $json_respones = json_encode($response);
-            echo $json_respones;
+            echo "<li>".'<input type="checkbox" name="checkBoxList[]" value="'.$row["id"].'"><span>'.$row["task"]."</span></li>";
         }
-       
+        echo '</ul>';
     } else {
-        echo json_encode("No record Found");
+        echo '<h2>Your ToDo list is empty!</h2>';
     }
 }
 ?>
